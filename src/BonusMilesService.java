4ac41d4b2f23e1f67468d177bcaa9693f75fefbc
Neto test <ki.dmitriev.yandex@gmail.com>
2024-09-24 16:14:46 +0300

    ticketPrise

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,12 +1,12 @@
 public class BonusMilesService {
-    public int calculate(int a) {
+    public int calculate(int ticketPrise) {
         // Проверям что цена билета положительная
-        if (a <= 0) {
+        if (ticketPrise <= 0) {
             return 0; // возвращаем 0 если цена некорректна
         }
 
         // Расчитываем количество миль
-        int miles = a / 20; // миля за каждые 20 рублей
+        int miles = ticketPrise / 20; // миля за каждые 20 рублей
         return miles;
     }
 }
